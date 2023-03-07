b27afc850b8135233bbd59b7568b813a8b8f177b
dev-rafa1707 <rafa1707@gmail.com>
2023-03-07 17:42:19 -0300

    Adding files server.js and package.json

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+
+const users = []
+
+app.get('/users',(req,res)=>{
+    res.json(users)
+})
+
+
+
+
+app.listen(3000,()=>{
+    console.log('Online server')
+})
