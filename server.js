943157bf375e58c5ed3d3448c5c1a2a9366e1f9d
dev-rafa1707 <rafa1707@gmail.com>
2023-03-08 23:35:56 -0300

    Adiciona request.rest e incrementa rotas no servidor

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require("express")
 const app = express()
+const bcrypt = require('bcrypt')
 
-const users = [{name:'Name'}]
+app.use(express.json())
+
+const users = []
 
 app.get('/users',(req,res)=>{
     res.json(users)
 })
 
-app.post('/users', (req,res)=>{
-     
+app.post('/users', async (req,res)=>{
+    try {
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const user = {name: req.body.name, password: hashedPassword}
+        users.push(user)
+        res.status(201).send() 
+    } catch{
+        res.status(500).send()
+    }
+    
+    
 })
 
 
